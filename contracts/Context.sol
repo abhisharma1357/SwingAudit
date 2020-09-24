d92fba8435eb14493034de812778ad8e4fcca9eb
abhi21094 <abhisharma.sharma7@gmail.com>
2020-09-24 23:17:53 +0530

    Smart contract Audit

---
+++ contracts/Context.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.16;
+contract Context {
+    // Empty internal constructor, to prevent people from mistakenly deploying
+    // an instance of this contract, which should be used via inheritance.
+    constructor () internal { }
+    // solhint-disable-previous-line no-empty-blocks
+
+    function _msgSender() internal view returns (address payable) {
+        return msg.sender;
+    }
+
+} // File: @openzeppelin/contracts/GSN/Context.sol
